e623ef642658e9c47cacd17be5434b7222e2db38
atsougan870@gmail.com <atsougan870@gmail.com>
2024-08-30 09:53:45 +0300

    Add Login Signup pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import ShopContextProvider from './Context/ShopContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode> 
-  <App />
-  </React.StrictMode> 
+  <ShopContextProvider>
+    <App />
+  </ShopContextProvider>
+
 );
