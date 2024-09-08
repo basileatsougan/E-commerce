6118ec44cea499a7a0df804deb6a4a00e4e26a93
atsougan870@gmail.com <atsougan870@gmail.com>
2024-09-09 02:03:28 +0300

    Add product pages

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext';
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find((e)=> e.id === Number(productId));
+
   return (
-    <div>Product</div>
+    <div className='product'>
+      <Breadcrum product={product}/>
+      <ProductDisplay product={product}/>
+    </div>
   )
 }
 
